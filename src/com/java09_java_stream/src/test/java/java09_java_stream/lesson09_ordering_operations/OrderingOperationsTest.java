cce519b669560d1538655aa332709758ccbc9148
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-01-22 17:15:42 +0700

    ordering operations

---
+++ src/com/java09_java_stream/src/test/java/java09_java_stream/lesson09_ordering_operations/OrderingOperationsTest.java
@@ -0,0 +1,41 @@
+package java09_java_stream.lesson09_ordering_operations;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Ordering Operations
+ *
+ * - Java Stream juga mendukung operasi untuk melakukan pengurutan data Stream nya
+ * - Secara default, data akan diurutkan mengikuti Comparable yang terdapat di data yang ada di Stream
+ * - Jika kita ingin mengurutkan secara manual, kita bisa menggunakan Comparator sendiri
+ *
+ * Method Retrieving Single Element
+ *
+ * |----------------------------------------------------------------|
+ * | Method             | Keterangan                                |
+ * |----------------------------------------------------------------|
+ * | sorted()           | Mengurutkan berdasarkan comparable data   |
+ * | sorted(Comparator) | Mengurutkan berdasarkan comparator        |
+ * |----------------------------------------------------------------|
+ */
+public class OrderingOperationsTest {
+
+    @Test
+    void testSorted() {
+        List.of("Adnin", "Rifandi", "Sutanto").stream()
+                .sorted()
+                .forEach(System.out::println); // Adnin, Rifandi, Sutanto
+    }
+
+    @Test
+    void testSortedWithComparator() {
+        Comparator<String> reverseComparator = Comparator.reverseOrder();
+
+        List.of("Adnin", "Rifandi", "Sutanto").stream()
+                .sorted(reverseComparator)
+                .forEach(System.out::println); // Sutanto, Rifandi, Adnin
+    }
+}
