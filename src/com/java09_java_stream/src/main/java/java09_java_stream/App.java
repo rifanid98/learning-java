7f70d15d22562e9f8b453a0b02cc1afa7c980016
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-01-22 15:59:36 +0700

    initial commit - java stream

---
+++ src/com/java09_java_stream/src/main/java/java09_java_stream/App.java
@@ -0,0 +1,13 @@
+package java09_java_stream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
