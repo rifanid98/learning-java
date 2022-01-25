35b19a0fd3b5e33cb81fffcd29a4ded273cb752f
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-01-25 09:54:53 +0700

    initial commit - java date & time

---
+++ src/com/java12_date_time/src/main/java/java12_date_time/App.java
@@ -0,0 +1,13 @@
+package java12_date_time;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
