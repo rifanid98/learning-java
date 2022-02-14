21879331894e6118564e9d8d307db374aa895505
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-02-14 23:40:09 +0700

    singleton

---
+++ src/com/java18_spring_dasar/src/test/java/java18_spring_dasar/lesson03_singleton/Database.java
@@ -0,0 +1,15 @@
+package java18_spring_dasar.lesson03_singleton;
+
+public class Database {
+    private static Database database;
+
+    public static Database getInstance() {
+        if (database == null) {
+            database = new Database();
+        }
+        return database;
+    }
+
+    private Database() {
+    }
+}
