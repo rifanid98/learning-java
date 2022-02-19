c5f51bc953ab4dbc6375ab506ee0d01841ae07ea
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-02-19 19:54:09 +0700

    aware

---
+++ src/com/java18_spring_dasar/src/test/java/java18_spring_dasar/lesson30_aware/BeanConfiguration.java
@@ -0,0 +1,9 @@
+package java18_spring_dasar.lesson30_aware;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(AuthService.class)
+public class BeanConfiguration {
+}
