f6ba8b10f5dddc112a2b1156d0611eab2b2fddc5
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-02-19 14:16:40 +0700

    lifecycle annotation

---
+++ src/com/java18_spring_dasar/src/test/java/java18_spring_dasar/lesson16_lifecycle_annotation/BeanConfiguration.java
@@ -0,0 +1,13 @@
+package java18_spring_dasar.lesson16_lifecycle_annotation;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server server() {
+        return new Server();
+    }
+}
