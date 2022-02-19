1eb145ccb207e54428baa7b25d0f563ae71f9730
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-02-19 14:06:08 +0700

    lifecycle

---
+++ src/com/java18_spring_dasar/src/test/java/java18_spring_dasar/lesson15_lifecycle/BeanConfiguration.java
@@ -0,0 +1,13 @@
+package java18_spring_dasar.lesson15_lifecycle;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public Connection connection() {
+        return new Connection();
+    }
+}
