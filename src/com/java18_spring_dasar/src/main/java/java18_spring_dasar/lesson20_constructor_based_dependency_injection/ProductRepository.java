e4614903e0b1775c4d881a78475ff764c785ef84
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-02-19 14:47:32 +0700

    constructor-based dependency injection

---
+++ src/com/java18_spring_dasar/src/main/java/java18_spring_dasar/lesson20_constructor_based_dependency_injection/ProductRepository.java
@@ -0,0 +1,10 @@
+package java18_spring_dasar.lesson20_constructor_based_dependency_injection;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Constructor-based Dependency Injection
+ */
+@Component
+public class ProductRepository {
+}
